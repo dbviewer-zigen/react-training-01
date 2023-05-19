e2de805035ccdf852174dde1ac7c77f6d66f3e06
zigen <dbviewer.zigen@gmail.com>
2023-05-20 08:36:16 +0900

    コメントを追加

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,24 +4,30 @@ import { CounterUpButton } from "./CounterUpButton";
 import { CountReducer } from "./CountReducer";
 
 // useStateの代わりにuseReducerを使う
+// useReducerのメリット(一部)
+//  ステートを更新するアクションを別の関数コンポで宣言できる。
+// 　- ロジックの単体テストが容易になる。
+//   - ソースの可読性が向上する.
+//   - 再レンダリング防止するための処理が容易になる。
 export const Counter = () => {
   //const [count, setCount] = useState(0);
-
   const [count, dispatch] = useReducer(CountReducer, 0);
 
-  const countDown = () => {
-    dispatch({ type: "decrement" });
-  };
-  const countUp = () => {
-    dispatch({ type: "increment" });
-  };
+  // const countDown = () => {
+  //   dispatch({ type: "decrement" });
+  // };
+  // const countUp = () => {
+  //   dispatch({ type: "increment" });
+  // };
 
   console.log("render Counter");
   return (
     <>
       <div>Counter:{count}</div>
-      <CounterDownButton countDown={countDown}></CounterDownButton>
-      <CounterUpButton countUp={countUp}></CounterUpButton>
+      {/* <CounterDownButton countDown={countDown}></CounterDownButton>
+      <CounterUpButton countUp={countUp}></CounterUpButton> */}
+      <CounterDownButton countDown={() => dispatch({ type: "decrement" })} />
+      <CounterUpButton countUp={() => dispatch({ type: "increment" })} />
     </>
   );
 };
