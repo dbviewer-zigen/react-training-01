232697bc769e772344abef43065e58c29476941f
zigen <dbviewer.zigen@gmail.com>
2023-05-20 09:10:38 +0900

    コメント追加

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -17,8 +17,9 @@ export const CountButtonArea = () => {
   // const [count, dispatch] = useReducer(CountReducer, 0);
 
   // CountContextからdispatchのみを取得
-  const { count, dispatch } = useContext(CountContext);
+  const { dispatch } = useContext(CountContext);
 
+  // Contextからdispachだけを取得していても、再描画がされるためuseMemoを使いメモ化する
   //return <DispatchButton dispatch={dispatch} />;
   return useMemo(() => {
     return (
@@ -30,13 +31,13 @@ export const CountButtonArea = () => {
   }, [dispatch]);
   // useMemoの第二引数について
   // 第二奇数の意味 依存配列
-  // ・依存配列の要素の値が変わると再計算(レンダリング)される
-  // ・依存配列を[]にすると、関数コンポーネントが初めて描画される時だけ計算(レンダリング)する
+  // ・依存配列の要素の値が変わると再計算(描画)される
+  // ・依存配列を[]にすると、関数コンポーネントが初めて描画される時だけ計算(描画)する
 
   // 今回のケース(useReducerを使っているケース)
-  // ・依存配列にcountを設定すると、count値が変わる事に再レンダリングする・・・useMemoの意味がない
-  // ・依存配列にdispathをを設定しても、dispath値は変わらないので、再レンダリングされない・・・期待どおり
-  // ・依存配列に[]を設定すると、初回のみレンダリングする・・・期待どおり（推奨されない実装）
+  // ・依存配列にcountを設定すると、count値が変わる事に再描画する・・・useMemoの意味がない
+  // ・依存配列にdispathをを設定しても、dispath値は変わらないので、再描画されない・・・期待どおり
+  // ・依存配列に[]を設定すると、初回のみ描画する・・・期待どおり（推奨されない実装）
 };
 
 // // dispatch を Props として受け取るコンポーネントをメモ化し、不要な再レンダリングを防ぐ
