3fc8a34aab83bfc7289de103197fddef3638a83b
zigen <dbviewer.zigen@gmail.com>
2023-05-21 10:04:50 +0900

    Todoを追加する処理

--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useTodo } from "../hooks/useTodo";
 
 // Todoリストを一覧で表示するサンプル
@@ -8,9 +8,23 @@ export const TodoListPage = () => {
   // 第一引数はステート、第二引数以降はステートを更新するための各種関数
   const { todoList, add, update, remove } = useTodo();
 
+  // TextAreaの値を参照するためのuseRefを宣言する　<textarea>タグのref属性に設定
+  const inputElement = useRef<HTMLTextAreaElement>(null);
+
+  // 追加ボタンを押下した時の関数を宣言
+  const handleAdd = () => {
+    if (inputElement.current?.value === "") {
+      return; // 値が空の場合は何もしない
+    }
+    add(inputElement.current!.value, "test_user"); // カスタムフックのadd関数を使って登録する
+    inputElement.current!.value = ""; //登録後は空白を設定する
+  };
+
   console.log("render todo list");
   return (
     <div>
+      <textarea ref={inputElement} />
+      <button onClick={handleAdd}>追加</button>
       <ul>
         {todoList.map((todo, index) => (
           <li key={todo.id}>
