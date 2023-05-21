0f9a5e3bee7097e79fc1d5067b986c5f966dd634
zigen <dbviewer.zigen@gmail.com>
2023-05-21 09:07:51 +0900

    initial commit

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 // export const useFetch = <T,>({ url, config }: { url: string; config: any }) => {
 export const useFetch = <T,>(url: string) => {
