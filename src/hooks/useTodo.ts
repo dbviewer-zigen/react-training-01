5417884e0c914e37c4d26777d22a5c875480d369
zigen <dbviewer.zigen@gmail.com>
2023-05-21 09:31:57 +0900

    commit

---
+++ src/hooks/useTodo.ts
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import * as todoApi from "../apis/todos";
+import { Todo } from "../types/Todo";
+import { ulid } from "ulid";
+// Environmental requirements
+// - install ulid, axios
+// カスタムHook
+export const useTodo = () => {
+  // useStateの引数に何を渡すのか？ useState() フックの唯一の引数は state の初期値です。
+  // useState は何を返すのか？ 現在の state と、それを更新するための関数とを、ペアにして返します。
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  // Update the document title using the browser API
+  useEffect(() => {
+    console.log("useTodo# useEffect");
+    todoApi.findAll().then((todos) => {
+      setTodoList(todos); // 検索結果をそのまま設定
+      // ex 登録順の逆順で表示する場合
+      // setTodoList([...todos].reverse()); // リバース（逆順）して設定
+    });
+  }, []);
+
+  // Todoの登録
+  const add = (title: string, userId: string) => {
+    // あたらしいitemを作成する
+    const newItem: Todo = {
+      id: ulid(),
+      title: title,
+      completed: false,
+      userId: userId,
+    };
+
+    // サーバーの追加APIを呼ぶ
+    todoApi.add(newItem).then((addTodo) => {
+      // --------------------------------
+      // 新しいtodoListをstateにセットする
+      // --------------------------------
+      // addTodoをtodoListの先頭に追加、その後todoListを展開
+      // ...はスプレット演算子(オブジェクトの展開)
+      setTodoList([addTodo, ...todoList]);
+    });
+  };
+
+  // IDを指定してタイトルを更新する
+  const update = (id: string, title: string) => {
+    // todoListから、idが一致する1件を取り出す
+    const todoItem = todoList.find((item: Todo) => item.id === id);
+    // タイトルを上書きする
+    const updateItem: Todo = { ...todoItem!, title: title };
+    // サーバに更新API呼ぶ
+    todoApi.update(id, updateItem).then((updatedTodo) => {
+      // 更新が成功したら、todoListを更新する
+      // 保持しているtodoListからidが一致しているTodoをサーバーから返ってきたupdatedTodoで更新する
+      const newTodoList = todoList.map((item) =>
+        item.id !== id ? item : updatedTodo
+      );
+      // --------------------------------
+      // 新しいtodoListをstateにセットする
+      // --------------------------------
+      setTodoList(newTodoList);
+    });
+  };
+
+  // 指定したIDのデータを削除する
+  const remove = (id: string) => {
+    todoApi.remove(id).then((deletedid) => {
+      const newTodoList = todoList.filter((item) => item.id !== deletedid);
+      // --------------------------------
+      // 削除した新しいtodoListをstateにセットする
+      // --------------------------------
+      setTodoList(newTodoList);
+    });
+  };
+
+  // 作成した関数を返す
+  // 第一引数:ステート
+  // 第二引数以降:ステートを更新するための関数
+  return {
+    todoList,
+    add,
+    update,
+    remove,
+  };
+};
--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,51 +1,18 @@
-import React, { useState } from "react";
-import { useFetch } from "../hooks/useFetch";
-
-export type Todo = {
-  id: string;
-  title: string;
-  completed: boolean;
-  userId: string;
-};
+import React from "react";
+import { useTodo } from "../hooks/useTodo";
 
 // Todoリストを一覧で表示するサンプル
 export const TodoListPage = () => {
-  // const todos = [
-  //   { id: "aaa", title: "aaa", completed: false, userId: "user01" },
-  //   { id: "bbb", title: "bbb", completed: true, userId: "user01" },
-  //   { id: "ccc", title: "ccc", completed: false, userId: "user01" },
-  //   { id: "ddd", title: "ddd", completed: false, userId: "user01" },
-  //   { id: "eee", title: "eee", completed: false, userId: "user01" },
-  //   { id: "fff", title: "fff", completed: false, userId: "user01" },
-  //   { id: "ggg", title: "ggg", completed: false, userId: "user01" },
-  // ] as Todo[]; // 型を設定する
-
-  // urlを指定してAPIサーバからTodo[]を取得する
-  const url = "https://jsonplaceholder.typicode.com/todos";
-  const { data, isLoading, error } = useFetch<Todo[]>(url); // 型を指定する
-
-  if (isLoading) {
-    return <h4>Loading...</h4>;
-  }
-
-  if (error) {
-    // alert(error);
-    console.log(error);
-    return (
-      <>
-        <h4>Error Occured</h4>
-      </>
-    );
-  }
+  // カスタムフックから必要な変数を取得
+  // useTodoというカスタムフックを宣言
+  // 第一引数はステート、第二引数以降はステートを更新するための各種関数
+  const { todoList } = useTodo();
 
   console.log("render todo list");
-
-  // todos.mapを使って繰り返し処理を行う
-  //  - 子要素のkey属性に一意の値を格納する　key={todo.id}
   return (
     <div>
       <ul>
-        {data.map((todo, index) => (
+        {todoList.map((todo, index) => (
           <li key={todo.id}>
             {todo.title} {todo.completed ? "(完了)" : ""}
           </li>
